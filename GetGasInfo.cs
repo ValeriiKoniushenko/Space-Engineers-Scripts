4dd7451360126a757bc432267450a577f73ce6c4
ValeriiKoniushenko <v.koniushenko@pinglestudio.com>
2023-03-25 19:21:58 +0200

    Updated gas info

--- GetGasInfo.cs
+++ GetGasInfo.cs
@@ -1,6 +1,78 @@
+class MyCanvas
+{
+	private const int LCDWidth = 35;
+	private const int LCDHeight = 17;
+	private int HCurret = 1;
+	private List<IMyTextPanel> LCDs = new List<IMyTextPanel>();
+
+	public void AddLCD(IMyTextPanel LCD)
+	{
+		LCDs.Add(LCD);
+	}
+
+	public void WriteLine(string Text)
+	{
+		int Index = HCurret / LCDHeight;
+		if (Index < LCDs.Count)
+		{
+			LCDs[Index].WriteText(Text + "\n\r", true);
+			++HCurret;
+		}
+	}
+	public void Write(string Text)
+	{
+		int Index = HCurret / LCDHeight;
+		if (Index < LCDs.Count)
+		{
+			LCDs[Index].WriteText(Text, true);
+		}
+	}
+
+	public void Clear()
+	{
+		foreach (var LCD in LCDs)
+		{
+			LCD.WriteText("");
+		}
+		HCurret = 1;
+	}
+
+	public void MakeProgressBar(double Load)
+	{
+		Write("[ ");
+		double Width = LCDWidth * 2.43;
+		for(double i = 1.0; i <= Width; ++i)
+		{
+			Write(i / Width >= Load ? "'" : "|");
+		}
+		Write(" ]");
+		WriteLine("");
+	}
+}
+
 public void Main(string args)
 {
-	IMyTextPanel LCD = GridTerminalSystem.GetBlockWithName("HydrogenLCD") as IMyTextPanel;
+	string[] arguments = args.Trim().Split(' ');
+	var Canvas = new MyCanvas();
+
+	if (arguments.Length < 1)
+	{
+		Echo("You have to pass a name almost one LCD panel, or more to get some result");
+		return;
+	}
+
+	for (int i = 0; i < arguments.Length; ++i)
+	{
+		var LCD = GridTerminalSystem.GetBlockWithName(arguments[i]) as IMyTextPanel;
+		if (LCD == null)
+		{
+			Echo("Can't find a LCD panel with name: " + arguments[i]);
+			return;
+		}
+		Canvas.AddLCD(LCD);
+	}
+
+	Canvas.Clear();
 
 	List<IMyGasTank> Tanks = new List<IMyGasTank>();
 	if (Tanks == null)
@@ -8,13 +80,9 @@ public void Main(string args)
 
 	GridTerminalSystem.GetBlocksOfType<IMyGasTank>(Tanks);
 
-	LCD.WriteText("======= | Gas Tanks | ========\n\r");
-	double Ratios = 0;
-	for (int i = 0; i < Tanks.Count; ++i)
+	foreach (var Tank in Tanks)
 	{
-		LCD.WriteText("Tank "+ "'" + Tanks[i].CustomName + "'" + " #" + i + ": " + (int)(Tanks[i].FilledRatio * 100.0) + "% \n\r", true);
-		Ratios += Tanks[i].FilledRatio;
+		Canvas.WriteLine(Tank.CustomName + ": " + (int)(Tank.FilledRatio * 100.0) + "%");
+		Canvas.MakeProgressBar(Tank.FilledRatio);
 	}
-
-	LCD.WriteText("Total: " + (int)(Ratios / Tanks.Count * 100.0) + "% \n\r", true);
 }
